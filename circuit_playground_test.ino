c50e34d5de9580d069175bf72b12ff382189d26e
Richard <rgbailey@gmail.com>
2016-07-28 15:30:07 -0700

    finish light sensor and add sleep mode.

--- circuit_playground_test.ino
+++ circuit_playground_test.ino
@@ -1,10 +1,10 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <FastLED.h>
-
+#include <Adafruit_SleepyDog_Library/Adafruit_SleepyDog.h>
 #define NUM_LEDS 10
 #define DATA_PIN 17
 unsigned long last_start;
-unsigned int new_led_delay = 750;
+unsigned int new_led_delay = 650;
 unsigned int fade_delay = 50;
 
 CRGB leds[NUM_LEDS];
@@ -30,15 +30,21 @@ void setup()
 
 void loop()
 {
+	while (!CircuitPlayground.slideSwitch()) {
+		// Turn off the pixels, then go into deep sleep for a second.
+		FastLED.clear();
+		FastLED.show();
+		Watchdog.sleep(1000);
+	}
 
 	fadeToBlackBy( leds, NUM_LEDS, 10);
 	if (millis()-last_start > new_led_delay)
 	{
 		FastLED.setBrightness(constrain(map (CircuitPlayground.lightSensor(),0,1023,0,255),10,255));
-		int x = random16(NUM_LEDS);
+		byte x = random16(NUM_LEDS);
 		if (!leds[x])
 		{
-			leds[ x ] += CHSV( random8(), random8( 192, 255), 255);
+			leds[ x ] += CHSV( random8(), random8( 128, 255), 255);
 			last_start = millis();
 		}
 	}
