02ce5bb203febd4a8078497a5f16de46327e1c57
Richard <rgbailey@gmail.com>
2016-07-27 21:00:03 -0700

    initial commit

---
+++ circuit_playground_test.ino
@@ -0,0 +1,45 @@
+#include <Adafruit_CircuitPlayground.h>
+#include "FastLED.h"
+
+#define NUM_LEDS 10
+#define DATA_PIN 17
+unsigned long last_start;
+unsigned int new_led_delay = 750;
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+
+	CircuitPlayground.begin();
+
+	// sanity check delay - allows reprogramming if accidentally blowing power w/leds
+	delay(3000);
+
+
+	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection(0xB0FFFF);
+	
+	// set master brightness to enable FastLED dithering
+	FastLED.setBrightness(64);
+	last_start = millis();
+
+
+}
+
+void loop()
+{
+
+	fadeToBlackBy( leds, NUM_LEDS, 10);
+	if (millis()-last_start > new_led_delay)
+	{
+		int x = random16(NUM_LEDS);
+		if (!leds[x])
+		{
+			leds[ x ] += CHSV( random8(), random8( 192, 255), 255);
+			last_start = millis();
+		}
+	}
+	FastLED.show();
+	FastLED.delay(50);
+	
+}
